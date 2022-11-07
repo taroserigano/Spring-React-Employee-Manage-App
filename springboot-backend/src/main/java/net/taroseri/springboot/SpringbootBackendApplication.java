f836d848a63613b2f1d24abfcd0a4319c4e36bec
Taro Serigano <taroserigano@gmail.com>
2022-11-07 17:14:44 -0600

    Add files via upload

---
+++ springboot-backend/src/main/java/net/taroseri/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package net.taroseri.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+}
